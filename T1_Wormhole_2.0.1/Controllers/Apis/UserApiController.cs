c0683803c87bd2f7b245ef6213ff49f820ec7877
Borneol <a1991818@gmail.com>
2025-04-16 00:43:16 +0800

    add test getPhoto api

--- T1_Wormhole_2.0.1/Controllers/Apis/UserApiController.cs
+++ T1_Wormhole_2.0.1/Controllers/Apis/UserApiController.cs
@@ -27,7 +27,7 @@ namespace T1_Wormhole_2._0._1.Controllers.Apis
             //    Name = x.Name,
             //}).ToList();
 
-            var result = _db.UserInfos.Where(x => x.UserId == id);            
+            var result = _db.UserInfos.Where(x => x.UserId == id);
             return result;
         }
         [HttpGet]
@@ -129,17 +129,80 @@ namespace T1_Wormhole_2._0._1.Controllers.Apis
         //        }
         //    }
         //}
-        [HttpPost]
-        public bool AddPhoto(UserInfoDto model)
-        {
-            var data = _db.UserInfos.FirstOrDefault(y => y.UserId == model.UserId);
-            if (data == null)
-            {
-                return false;
-            }
-            data.Photo = model.Photo;
-            _db.SaveChanges();
-            return true;
-        }
+        //[HttpPost]
+        //public bool AddPhoto(UserInfoDto model)
+        //{
+        //    var data = _db.UserInfos.FirstOrDefault(y => y.UserId == model.UserId);
+        //    if (data == null)
+        //    {
+        //        return false;
+        //    }
+        //    data.Photo = model.Photo;
+        //    _db.SaveChanges();
+        //    return true;
+        //}
+
+        //4/15 晚上改動
+        //[HttpGet]
+        //public async Task<IEnumerable<UserInfo>> Get(int id)
+        //{
+        //    var result = _db.UserInfos.Where(x => x.UserId == id)
+        //        .Select(e => new UserInfo
+        //        {
+        //            UserId = e.UserId,
+        //            Name = e.Name,
+        //            Nickname = e.Nickname,
+        //            Phone = e.Phone,
+        //            Brithday = e.Brithday,
+        //            SignatureLine = e.SignatureLine,
+        //            Sex = e.Sex,
+        //            Photo = null,
+        //        });
+        //    return result;
+        //}
+        //[HttpGet]
+        //public async Task<IEnumerable<UserStatus>> GetStatus(int id)
+        //{
+        //    var result = _db.UserStatuses.Where(x => x.Id == id);
+        //    return result;
+        //}
+
+        //[HttpGet("GetPhoto/{id}")]
+        //public async Task<FileResult> GetPhoto(int id)
+        //{
+        //    string fileName = Path.Combine("wwwroot", "images", "200Test.jpg");
+        //    UserInfo e = await _db.UserInfos.FindAsync(id);
+        //    byte[]? ImageContent = e?.Photo != null ? e.Photo : System.IO.File.ReadAllBytes(fileName);
+        //    return File(ImageContent, "image/jpeg");
+        //}
+
+        //[HttpPost]
+        //public async Task<string> Edit(UserInfoDto model)
+        //{
+        //    UserInfo data = await _db.UserInfos.FindAsync(model.UserId);
+        //    if (data == null)
+        //    {
+        //        return "找不到會員資料";
+        //    }
+        //    data.Name = model.Name;
+        //    data.Nickname = model.Nickname;
+        //    data.Brithday = DateOnly.Parse(model.Birthday);
+        //    data.SignatureLine = model.Signature;
+        //    data.Sex = model.Sex;
+        //    //data.Phone = model.Phone;
+        //    if (model.Photo != null)
+        //    {
+        //        using (BinaryReader br = new BinaryReader(model.Photo.OpenReadStream()))
+        //        {
+        //            data.Photo = br.ReadBytes((int)model.Photo.Length);
+        //        }
+        //    }
+
+        //    _db.Entry(data).State = EntityState.Modified;
+
+        //    //_db.Update(data);
+        //    await _db.SaveChangesAsync();
+        //    return (data.ToString());
+        //}
     }
 }
