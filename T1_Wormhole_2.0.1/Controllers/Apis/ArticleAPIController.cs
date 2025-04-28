1022760ef3cdd606fde545a192260ef929ea1768
Sam <fantasy871014@gmail.com>
2025-04-28 16:05:41 +0800

    暫時紀錄

--- T1_Wormhole_2.0.1/Controllers/Apis/ArticleAPIController.cs
+++ T1_Wormhole_2.0.1/Controllers/Apis/ArticleAPIController.cs
@@ -4,6 +4,7 @@ using T1_Wormhole_2._0._1.Models.Database;
 using Microsoft.EntityFrameworkCore;
 using T1_Wormhole_2._0._1.Models;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using T1_Wormhole_2._0._1.Models.DTOs;
 
 namespace T1_Wormhole_2._0._1.Controllers
 {
@@ -25,10 +26,10 @@ namespace T1_Wormhole_2._0._1.Controllers
             .Where(r => (r.ArticleId == articleId))
             .Select(r => new
             {
-                 writer = r.User.Nickname,
-                 content = r.Comment,
+                writer = r.User.Nickname,
+                content = r.Comment,
                 date = r.CreateTime.ToString("yyyy年MM月dd日 HH點mm分ss秒")
-                
+
             }).ToList();
 
 
@@ -37,8 +38,8 @@ namespace T1_Wormhole_2._0._1.Controllers
         [HttpGet]
         public IActionResult GetRating(int articleId)
         {
-            
-            int? Ratingcount = _context.Ratings.Where(r => r.ArticleId == articleId).Select(x=>x.PositiveRating).Sum();
+
+            int? Ratingcount = _context.Ratings.Where(r => r.ArticleId == articleId).Select(x => x.PositiveRating).Sum();
             int? nRatingcount = _context.Ratings.Where(r => r.ArticleId == articleId).Select(x => x.NegativeRating).Sum();
             //int?[] Total =new int?[] {Ratingcount, nRatingcount };
             //Console.Write(Total);
@@ -47,7 +48,7 @@ namespace T1_Wormhole_2._0._1.Controllers
                 Positive = Ratingcount ?? 0,
                 Negative = nRatingcount ?? 0
             });
-            
+
         }
         [HttpGet]
         public async Task<FileResult> GetArticlePhoto(int articleId)
@@ -57,5 +58,80 @@ namespace T1_Wormhole_2._0._1.Controllers
             byte[] ImageContent = e?.Picture != null ? e.Picture : System.IO.File.ReadAllBytes(fileName);
             return File(ImageContent, "image/jpeg");
         }
+        [HttpPost]
+        public async Task<IActionResult> SubmitRating([FromBody] RatingRequest request)
+        {
+            // 查找是否有該文章的評價記錄
+            var rating = await _context.Ratings
+                .FirstOrDefaultAsync(r => r.ArticleId == request.ArticleId);
+
+            if (rating == null)
+            {
+                // 如果沒有記錄，新增一筆
+                rating = new Rating
+                {
+                    ArticleId = request.ArticleId,
+                    PositiveRating = request.IsPositive ? 1 : 0,
+                    NegativeRating = request.IsPositive ? 0 : 1
+                };
+                _context.Ratings.Add(rating);
+            }
+            else
+            {
+                //更新喜歡或不喜歡
+                if (request.IsPositive)
+                {
+                    rating.PositiveRating = (rating.PositiveRating ?? 0) + 1;
+                }
+                else
+                {
+                    rating.NegativeRating = (rating.NegativeRating ?? 0) + 1;
+                }
+            }
+
+            // 儲存變更到資料庫
+            await _context.SaveChangesAsync();
+
+            // 回傳成功訊息
+            return Ok(new { success = true });
+
+        }
+        public class RatingRequest
+        {
+            public int ArticleId { get; set; }
+            public bool IsPositive { get; set; }
+        }
+        /// <summary>
+        /// 新增留言
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> ArticleResponse([FromBody] ArticleResponse request)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(request.Comment))
+                {
+                    return BadRequest("留言內容不能為空白");
+                }
+
+                var newResponse = new ArticleResponse
+                {
+                    ArticleId = request.ArticleId,
+                    Comment = request.Comment,
+                    UserId = request.UserId,   // 這裡必須傳正確的UserId
+                    CreateTime = DateTime.Now
+                };
+
+                _context.ArticleResponses.Add(newResponse);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { success = false, message = ex.Message });
+            }
+        }
     }
+    
 }
