cdfe62b43875dfeca336aaa052441a5e0ab3650f
LAPTOP-ISMAQJK8\G533QS <fantasy871014@gmail.com>
2025-04-29 17:02:11 +0800

    update to master 4dd9e6df514c3d6a4633fa8448925e6d6da3d056

---
+++ T1_Wormhole_2.0.1/Controllers/Apis/RelationApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using T1_Wormhole_2._0._1.Models.Database;
+
+namespace T1_Wormhole_2._0._1.Controllers.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RelationApiController : ControllerBase
+    {
+        private readonly WormHoleContext _db;
+
+        public RelationApiController(WormHoleContext db)
+        {
+            _db = db;
+        }
+
+        
+
+    }
+}
