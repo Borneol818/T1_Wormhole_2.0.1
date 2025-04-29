a9714ff2bcdda61bb75aa050357ca9a85fe11344
LAPTOP-ISMAQJK8\G533QS <fantasy871014@gmail.com>
2025-04-29 15:25:34 +0800

    更新Master e1255bb0c87177a91598e467b4a88f6319106b84

--- T1_Wormhole_2.0.1/Program.cs
+++ T1_Wormhole_2.0.1/Program.cs
@@ -43,25 +43,6 @@ builder.Services.AddHangfire(config => config
 builder.Services.AddHangfireServer();
 
 builder.Services.AddDbContext<WormHoleContext>(x => x.UseSqlServer(conStr));
-// 添加 Hangfire 服務
-builder.Services.AddHangfire(config => config
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UseSqlServerStorage(conStr));
-
-builder.Services.AddHangfireServer();
-
-
-// 添加 Hangfire 服務
-builder.Services.AddHangfire(config => config
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UseSqlServerStorage(conStr));
-
-builder.Services.AddHangfireServer();
-
 //這裡寫一個判定用的方法並存入在這裡new的變數名稱，用來當作登入後的認證跟各項頁面功能的全域變數
 var app = builder.Build();
 
@@ -80,8 +61,6 @@ app.UseRouting();
 app.UseAuthentication();
 
 app.UseAuthorization();
-// 啟用 Hangfire Dashboard
-app.UseHangfireDashboard("/hangfire");
 
 // 啟用 Hangfire Dashboard
 app.UseHangfireDashboard("/hangfire");
